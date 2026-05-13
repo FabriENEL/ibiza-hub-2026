892660fcc24e8d5d192679954ab5f37238831f13
Fabrizio Panighi <fabrizio.panighi@gmail.com>
2026-05-13 22:21:38 +0200

    Fix metadata e viewport per deploy

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,14 @@
 export const metadata = {
-      manifest: '/manifest.json',
-      themeColor: '#eab308',
-    }
-    ```
+    import type { Metadata, Viewport } from "next";
+
+// I metadati ora gestiscono solo le informazioni descrittive
+export const metadata: Metadata = {
+  title: "Ibiza Hub 2026",
+  description: "Alessandro's Bachelor Party",
+  manifest: "/manifest.json",
+};
+
+// Il colore del tema deve essere esportato separatamente tramite 'viewport'
+export const viewport: Viewport = {
+  themeColor: "#eab308",
+};
